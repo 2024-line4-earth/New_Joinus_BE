9b57c0672f7da92b530302c5f6a7c840f7e15d57
khoon9 <kshun08@dongguk.edu>
2025-05-08 02:04:23 +0900

    chore: models admin 등록
    - share model들 admin site에 등록

--- share/admin.py
+++ share/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    SharedCard,
+    Comment,
+    CardLike,
+    CardReport,
+)
 
-# Register your models here.
+admin.site.register(SharedCard)
+admin.site.register(Comment)
+admin.site.register(CardLike)
+admin.site.register(CardReport)
