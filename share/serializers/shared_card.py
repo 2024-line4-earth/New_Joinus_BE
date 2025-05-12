34dd0b76035b42a7654c9eb4906d657e55acf4a3
khoon9 <kshun08@dongguk.edu>
2025-05-12 16:14:23 +0900

    chore: 예외처리 추가
    - 로그인 사용자가 본인 소유가 아닌 실천카드에 대해 공유(게시)를 요청했을 때 예외 발생하도록 수정

--- share/serializers/shared_card.py
+++ share/serializers/shared_card.py
@@ -17,11 +17,20 @@ class SharedCardSerializer(serializers.ModelSerializer):
         fields = ["id", "user", "cardpost", "cardpost_id", "description", "created_at"]
         read_only_fields = ["created_at"]
 
+    def validate(self, attrs):
+        # 현재 로그인한 사용자
+        user = self.context["request"].user
+        cardpost = attrs.get("cardpost")
+
+        if cardpost.user != user:
+            raise serializers.ValidationError({"non_field_errors": ["자신이 작성한 카드만 공유(게시) 할 수 있습니다."]})
+        return attrs
+
     def create(self, validated_data):
         try:
             shared_card = SharedCard.objects.create(**validated_data)
         except IntegrityError:
-            raise serializers.ValidationError("이미 공유된 카드입니다.")
+            raise serializers.ValidationError({"non_field_errors": ["이미 공유된 카드입니다."]})
         
         PointService.add(shared_card.user, SCC.SHAREDCARD_CREATE_POINT, "공유 보상")
         return shared_card
