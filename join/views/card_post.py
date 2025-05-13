50ad592febaa5d7fa66fee8642a68d0e9589ef5e
khoon9 <kshun08@dongguk.edu>
2025-05-13 19:05:30 +0900

    feat: 실천카드 단일조회 api 추가
    - 실천카드 단일조회 api 추가
    - 소유자가 아닌 다른 유저가 단일조회 시도할 경우 에러 발생

--- join/views/card_post.py
+++ join/views/card_post.py
@@ -1,10 +1,12 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import views, generics, permissions, status
 from rest_framework.response import Response
 from join.models import CardPost
 from join.serializers import CardPostSerializer
 from join.utils import increase_rank_score # 랭킹
 from join.services import TutorialStateService
-
+from django.utils.timezone import now
+from rest_framework.exceptions import PermissionDenied
 
 class CardPostApiView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -25,3 +27,13 @@ class CardPostApiView(views.APIView):
             increase_rank_score(card.user)
             return Response(CardPostSerializer(card, context={"request": request}).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CardPostDetailView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk):
+        cardpost = get_object_or_404(CardPost.objects.select_related("user"), pk=pk)
+        if request.user != cardpost.user:
+            raise PermissionDenied("자신이 작성한 실천카드만 조회할 수 있습니다.")
+        serializer = CardPostSerializer(cardpost)
+        return Response(serializer.data)
