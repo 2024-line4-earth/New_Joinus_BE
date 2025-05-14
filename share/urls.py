9738fb211c541b2308fc51213315ad0bb34eb0c7
khoon9 <kshun08@dongguk.edu>
2025-05-15 00:30:09 +0900

    feat: 댓글 조회 응답형식 수정
    - 댓글 작성자 정보 포함하도록 수정

--- share/urls.py
+++ share/urls.py
@@ -6,21 +6,18 @@ from share.views.shared_card import (
     SharedCardDetailView,
     MySharedCardView,
 )
-from share.views.comment import CommentViewSet
+from share.views.comment import CommentView
 from share.views.card_like import CardLikeCreateDeleteView
 from share.views.card_report import CardReportCreateView
 from share.views.pin import PinnedSharedCardCreateDeleteView
 from share.views.stored_card import StoredCardCreateDeleteView
 from share.views.mission import MissionStateView
 
-router = DefaultRouter()
-router.register(r"comments", CommentViewSet, basename="comment")
-
 urlpatterns = [
-    path("", include(router.urls)),
     path("sharedcards/", SharedCardView.as_view(), name="sharedcard-list-and-create"),
     path("sharedcards/<int:pk>/", SharedCardDetailView.as_view(), name="sharedcard-detail"),
     path("sharedcards/my/", MySharedCardView.as_view(), name="my-sharedcard"),
+    path("comments/", CommentView.as_view(), name="comment"),
     path("likes/", CardLikeCreateDeleteView.as_view(), name="card-like"),
     path("reports/", CardReportCreateView.as_view(), name="card-report"),
     path("pins/", PinnedSharedCardCreateDeleteView.as_view(), name="pin"),
