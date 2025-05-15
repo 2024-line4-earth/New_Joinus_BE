0ee102f281c3338c4a3ed2b55fc39cd15c60d48e
bea816 <kimnadon@naver.com>
2025-05-15 17:52:49 +0900

    feat: 마켓 아이템 다운로드

--- market/urls.py
+++ market/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('itemlist/', ItemListAPIView.as_view()),
     path('item/<int:pk>/', ItemDetailAPIView.as_view()),
+    path('item/<int:pk>/download/', ItemDownloadView.as_view()),
 ]
